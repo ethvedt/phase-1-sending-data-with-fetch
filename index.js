b89ba335ec6a090b8f8dbbd792652d310d9a12d6
Eric Thvedt <ericsthvedt@gmail.com>
2023-02-03 15:21:04 -0800

    function submitData() works, but the tests keep failing and idk why

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const data = {
+        name: userName,
+        email: userEmail
+    }
+    const postObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+    return fetch("http://localhost:3000/users", postObj)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            //debugger;
+            const body = document.querySelector("body");
+            const p = document.createElement("p");
+            p.innerText = data.id;
+            body.appendChild(p);
+        })
+        .catch(err => {
+            const body = document.querySelector("body");
+            const p = document.createElement("p");
+            p.innerText = err.message;
+            body.appendChild(p);
+        });
+}
