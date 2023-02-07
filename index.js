9bb79d73c51451240f1b5b6786d363de4260c518
Eric Thvedt <ericsthvedt@gmail.com>
2023-02-07 10:39:21 -0800

    test 1 is failing but the code works. Wills helped me out

--- index.js
+++ index.js
@@ -11,21 +11,22 @@ function submitData(userName, userEmail) {
             "Accept": "application/json"
         },
         body: JSON.stringify(data)
-    }
+    };
     return fetch("http://localhost:3000/users", postObj)
         .then(res => res.json())
         .then(data => {
             console.log(data);
             //debugger;
             const body = document.querySelector("body");
-            const p = document.createElement("p");
-            p.innerText = data.id;
-            body.appendChild(p);
+            //const p = document.createElement("p");
+            body.textContent = data.id;
+            console.log(body);
+            //body.appendChild(p);
         })
         .catch(err => {
             const body = document.querySelector("body");
-            const p = document.createElement("p");
-            p.innerText = err.message;
-            body.appendChild(p);
+            //const p = document.createElement("p");
+            body.textContent = err.message;
+            //body.appendChild(p);
         });
 }
